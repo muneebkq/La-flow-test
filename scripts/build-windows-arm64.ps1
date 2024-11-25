d0e1cadc7771ea08736c4fc8c43d4032010ca913
muneebkq <muneebkq05@gmail.com>
2024-11-25 13:56:22 +0530

    fix: folder rename

--- scripts/build-windows-arm64.ps1
+++ scripts/build-windows-arm64.ps1
@@ -1,18 +1,19 @@
 $base_url = "https://dl.cryptlex.com/downloads"
 $lexactivator_version ="v3.30.3"
 
-New-Item -Path ".\tmp\windows" -ItemType Directory
+New-Item -Path ".\tmpArm64\windows" -ItemType Directory
 $url = "$base_url/$lexactivator_version/LexActivator-Win.zip"
 Write-Host "Downloading LexActivator library ..."
 Write-Host $url
-$output = ".\tmp\windows\LexActivator-Win.zip"
+$output = ".\tmpArm64\windows\LexActivator-Win.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 
-Expand-Archive $output -DestinationPath ".\tmp\windows\LexActivator-Win"
+Expand-Archive $output -DestinationPath ".\tmpArm64\windows\LexActivator-Win"
 
-Copy-Item -Path ".\tmp\windows\LexActivator-Win\libs\vc17\arm64\LexActivator.lib" -Destination ".\"
+Copy-Item -Path ".\tmpArm64\windows\LexActivator-Win\libss\vc17\arm64\LexActivator.lib" -Destination ".\"
 npm i
 node-gyp rebuild 
 
 Copy-Item -Path ".\build\Release\lexactivator.node" -Destination ".\lib\bindings\windows\arm64\lexactivator.node"
-Copy-Item -Path ".\tmp\windows\LexActivator-Win\libs\vc17\arm64\LexActivator.dll" -Destination ".\lib\bindings\windows\arm64\LexActivator.dll"
+Copy-Item -Path ".\tmpArm64\windows\LexActivator-Win\libs\vc17\arm64\LexActivator.dll" -Destination ".\lib\bindings\windows\arm64\LexActivator.dll"
+s
